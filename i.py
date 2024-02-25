5e8f5e4473bdfeab296f130fb955da0d73faaa8e
Haddouche Maamar <frocodeof@gmail.com>
2024-02-25 11:43:30 +0100

    Update

---
+++ i.py
@@ -0,0 +1,8 @@
+import os
+input_directory = r'D:\Programming\issues\Time-SocialMedia-Analysis\'
+
+if not os.path.exists(input_directory):
+    print(f"The directory does not exist: {input_directory}")
+else:
+    # Continue with your script
+    print("now you can continue with your script")
